99d3a4401f6f95c35beaacb9f5d7e9931d760edd
fatimesevilgen <fatimesevilgen@gmail.com>
2024-10-01 18:07:50 +0300

    update

---
+++ src/ProgramtheSumofOddNumbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ProgramtheSumofOddNumbers {
+    public static void main(String[] args) {
+
+        //
+        Scanner input = new Scanner(System.in);
+        int number, sum = 0;
+
+        do {
+            System.out.print("Bir sayı girin (çıkmak için tek sayı girin): ");
+            number = input.nextInt();
+
+            if (number % 2 == 0 && number % 4 == 0) {
+                sum += number;
+            }
+
+        } while (number % 2 == 0);
+
+        System.out.println("Çift ve 4'ün katı olan sayıların toplamı: " + sum);
+    }
+}
+
---
+++ src/ProgramtoFindEvenNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ProgramtoFindEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Bir sayı giriniz: ");
+        int number = scanner.nextInt();
+
+        int sum = 0;
+        int sayac = 0;
+
+        for (int i = 0 ; i <= number ; i++) {
+            if ((i % 3 == 0) && (i % 4 == 0)) {
+                sayac++;
+                sum += i;
+            }
+        }
+
+        if (sayac > 0) {
+            int average = sum / sayac;
+            System.out.println("Ortalama: " + average);
+        } else {
+            System.out.println("3 ve 4'e tam bölünen sayı bulunamadı.");
+        }
+    }
+}
